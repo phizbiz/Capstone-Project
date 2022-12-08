7126e82d7dbe0ed66605f08b0b157543d7d74b5d
Philip Basnight <philipbasnight@gmail.com>
2022-12-08 14:39:46 -0500

    routes updated

--- models/song.js
+++ models/song.js
@@ -3,8 +3,10 @@ const { Schema } = require('mongoose')
 const songSchema = new Schema(
   {
     name: { type: String, required: true },
-    url: { type: String, required: true }
-    // composer: [{ type: Schema.Types.ObjectId, ref: 'Composer', required: true }]
+    url: { type: String, required: false },
+    composer: [
+      { type: Schema.Types.ObjectId, ref: 'Composer', required: false }
+    ]
   },
   { timestamps: true }
 )
--- server.js
+++ server.js
@@ -6,15 +6,40 @@ const { Composer } = require('./models')
 
 const app = express()
 
+//middleware
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send({ msg: 'This route is being hit' })
 })
 
-//Song routes
+//SONG ROUTES
 
-//create song
-app.post('/songs', (req, res) => {
-  res.send({ msg: 'I hit the songs route' })
+//read all songs -- GET
+app.get('/songs', async (req, res) => {
+  let allSongs = await Song.find({})
+  res.json(allSongs)
+})
+
+//read one song -- GET
+app.get('/songs', async (req, res) => {
+  let oneSong = await Song.findOne({})
+  res.json(oneSong)
+})
+
+//create a song -- POST
+app.post('/songs', async (req, res) => {
+  let createdSong = await Song.create(req.body)
+  res.send(createdSong)
+})
+
+//delete a song -- SONG
+// app.delete('/songs/:id')
+
+//COMPOSER ROUTES
+app.post('/composer', async (req, res) => {
+  let createdComposer = await Composer.create(req.body)
+  res.send(createdComposer)
 })
 
 app.listen(PORT, () => {
