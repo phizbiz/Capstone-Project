1e1405f445afa9d1122c686c523695deae319659
Philip Basnight <philipbasnight@gmail.com>
2022-12-09 00:27:57 -0500

    CRUD functionality

--- client/src/components/updateSongs.js
+++ client/src/components/updateSongs.js
@@ -1,11 +1,96 @@
-const UpdateSongs = () => {
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import link from 'react-router-dom'
+import Songs from './Songs'
+
+const UpdateSongsPage = () => {
+  const [songs, updateSongs] = useState([])
+  const [tags, updateTags] = useState([])
+  const [formState, setFormState] = useState({
+    name: '',
+    url: '',
+    composer: '',
+    tag: ''
+  })
+
+  useEffect(() => {
+    const apiCall = async () => {
+      let response = await axios.get('http://localhost:3001/songs')
+      updateSongs(response.data)
+    }
+    apiCall()
+  }, [])
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    console.log(formState)
+    let newSong = await axios
+      .post('http://localhost:3001/songs', formState)
+      .then((response) => {
+        return response
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    updateSongs([...songs, newSong.data])
+    setFormState({ name: '', url: '', composer: '', tag: '' })
+  }
+  const handleUpdate = async (event, id) => {
+    event.preventDefault()
+    let updateSong = await axios
+      .put(`http://localhost:3001/songs/${id}`, formState)
+      .then((response) => {
+        return response
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+    updateSongs([...songs, updateSong.data])
+    setFormState({ name: '', url: '', composer: '', tag: '' })
+  }
+
   return (
-    <div>
+    <div className="updateSongs">
       <section>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name:</label>
+          <input id="name" value={formState.name} onChange={handleChange} />
+          <label htmlFor="url">URL:</label>
+          <input id="url" value={formState.url} onChange={handleChange} />
+          <label htmlFor="composer">Composer:</label>
+          <input
+            id="composer"
+            value={formState.composer}
+            onChange={handleChange}
+          />
+          <label htmlFor="tag">Tag:</label>
+          <input id="tag" value={formState.tag} onChange={handleChange} />
+          <button type="submit">Add Song</button>
+        </form>
+
         <h1>Update Songs Text</h1>
+        <div>
+          {songs.map((song) => (
+            <div key={song._id}>
+              <h3>Song: {song.name}</h3>
+              <h4>Tags: {song.tag}</h4>
+              <button
+                onClick={(event) => {
+                  handleUpdate(event, song._id)
+                }}
+                type="submit"
+              >
+                Update Song
+              </button>
+            </div>
+          ))}
+        </div>
       </section>
     </div>
   )
 }
-
-export default UpdateSongs
+export default UpdateSongsPage
