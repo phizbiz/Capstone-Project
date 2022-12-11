5f806ee4175541d285415db8eeb9151ef95452b3
Philip Basnight <philipbasnight@gmail.com>
2022-12-11 12:34:42 -0500

    tags mapped and linked on homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,36 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
 const Home = () => {
+  const [tags, updateTags] = useState([])
+
+  useEffect(() => {
+    const api = async () => {
+      let response = await axios.get('http://localhost:3001/tags')
+      updateTags(response.data)
+    }
+    api()
+  }, [])
+
   return (
-    <div classname="Home">
+    <div classname="allTags">
+      <header>
+        <h1>Royal Music Library</h1>
+      </header>
       <section>
-        <div>
-          <img src="images/Royal-Music-Library.png" />
+        <div className="divide">
+          {tags.map((tag) => {
+            return (
+              <Link to={`/${tag._id}`}>
+                <div className="tag" key={tag._ID}>
+                  <h2>{tag.name}</h2>
+                </div>
+              </Link>
+            )
+          })}
         </div>
         <h2>Royal Music Library</h2>
-        <h6>this text in the App dot js</h6>
         <h1>This is homepage text</h1>
       </section>
     </div>
