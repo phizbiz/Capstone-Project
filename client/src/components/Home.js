93e3ed5255641e1023b3df3e6e516f06b83c9a1d
Philip Basnight <philipbasnight@gmail.com>
2022-12-11 20:13:26 -0500

    style

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -16,7 +16,8 @@ const Home = () => {
   return (
     <div className="allTags">
       <header>
-        <h1>Royal Music Library</h1>
+        {/* <h1>Royal Music Library</h1> */}
+        <img src="https://i.imgur.com/bWtzMn7.png" className="logo"></img>
       </header>
       <section>
         <h2>Browse by genre...</h2>
@@ -24,15 +25,17 @@ const Home = () => {
           {tags.map((tag) => {
             return (
               <Link to={`tagID/${tag._id}`}>
-                <div className="tag" key={tag._ID}>
+                <div className="tagBox" key={tag._ID}>
                   <h2>{tag.name}</h2>
+                  <img
+                    src="https://i.imgur.com/pDYp0f2.jpg"
+                    className="tag-img"
+                  ></img>
                 </div>
               </Link>
             )
           })}
         </div>
-        <h2>Royal Music Library</h2>
-        <h1>This is homepage text</h1>
       </section>
     </div>
   )
