803e3190ccdc0dd843701d6e45c8495a17be05a6
Philip Basnight <philipbasnight@gmail.com>
2022-12-13 17:24:29 -0500

    FilmsDance

--- client/src/components/Films.js
+++ client/src/components/Films.js
@@ -5,13 +5,31 @@ const Films = () => {
     <div className="FilmPage">
       <h1>Pair our music with a classic movie scene...</h1>
       <div className="filmDiv">
-        <Link to="/action">
+        <Link to="/action" className="tagLinkyFilm">
           <img
             src="https://www.illustratedfiction.com/files/media/53796/matrixlobby.jpg"
             class="film-img"
           ></img>
           <h4>
             Action - <i>The Matrix</i>
+          </h4>
+        </Link>
+        <Link to="/dance" className="tagLinkyFilm">
+          <img
+            src="https://i.ytimg.com/vi/0aIR2oe6h-Y/maxresdefault.jpg"
+            class="film-img"
+          ></img>
+          <h4>
+            Dance - <i>Spider-Man</i>
+          </h4>
+        </Link>
+        <Link to="/romance" className="tagLinkyFilm">
+          <img
+            src="https://www.gannett-cdn.com/authoring/2016/06/10/NBCC/ghows-PA-22917d02-381c-43f1-8441-503d7f6a58c1-b7312b74.jpeg?width=1200&disable=upscale&format=pjpg&auto=webp"
+            class="film-img"
+          ></img>
+          <h4>
+            Romance - <i>Eternal Sunshine of the Spotless Mind</i>
           </h4>
         </Link>
       </div>
